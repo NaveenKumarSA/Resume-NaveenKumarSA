5b6fad724b54579fdc129e768dcec96e3f5ae2a3
NaveenKumarSA <naveen.souriraj@gmail.com>
2021-04-09 00:24:41 +0530

    first face of resume build on top of react application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import logo from './logo.svg';
+import "./App.css";
+import ObjectivesComponent from "./components/ObjectivesComponent";
+import ResumeAccordion from "./components/ResumeAccordion";       
+/* import ThemeSwitcher from './components/ThemeSwitcher';
+ <ThemeSwitcher/> */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="reasumeHeader ">
+        <img
+          src="/data/assets/NaveenKumarSA.jpg"
+          class="rounded mx-auto d-block"
+          alt="..."
+        ></img>   <p className="display-4">NAVEEN KUMAR S A</p>
+
+      </div>
+      <section>
+        <ObjectivesComponent />
+      </section>
+      <section>
+        <ResumeAccordion/>
+      </section>
+      <section>
+       
+      </section>
     </div>
   );
 }
---
+++ src/components/ResumeAccordion.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ProfTrainingTimeline from "./ProfTrainingTimeline";
+import EducationTabs from "./EducationTabs";
+
+const Accordion = withStyles({
+  root: {
+    border: "1px solid rgba(0, 0, 0, .125)",
+    boxShadow: "none",
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+    "&$expanded": {
+      margin: "auto",
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0, 0, 0, .03)",
+    borderBottom: "1px solid rgba(0, 0, 0, .125)",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiAccordionDetails);
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <Accordion
+        square
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>Exerience</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            I have been working as a Developer in iBridge Analysis and
+            development from July 2020- till date.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography>Professional Training </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <ProfTrainingTimeline />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Formal Education</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <EducationTabs />
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
